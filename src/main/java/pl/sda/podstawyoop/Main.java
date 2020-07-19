440e2dccddc5929653259fbb3fa22065ae63622d
Aleksandra Szafruga <ola.szafruga@gmail.com>
2020-07-19 12:26:06 +0200

    Point [2D, 3D], Shape [Rectangle, Triangle]

--- src/main/java/pl/sda/podstawyoop/Main.java
+++ src/main/java/pl/sda/podstawyoop/Main.java
@@ -3,24 +3,30 @@ package pl.sda.podstawyoop;
 public class Main {
 
     public static void main(String[] args) {
-        Point p0 = new Point();
-        Point p1 = new Point(2, 10);
-        Point p2 = new Point(-10, 20);
+        Point2D p0 = new Point2D();
+        Point2D p1 = new Point2D(2, 10);
+        Point2D p2 = new Point2D(-10, 20);
 
         comparePoints(p0, p1);
         comparePoints(p0, p2);
         comparePoints(p1, p2);
         comparePoints(p2, p1);
-        comparePoints(new Point(), p1);
-        comparePoints(new Point(), p2);
+        comparePoints(new Point2D(), p1);
+        comparePoints(new Point2D(), p2);
 
         Line l1 = new Line(p1, p2);
         System.out.println(l1);
         System.out.println("Długość lini to: " + l1.calculateLenght());
 
+        Point3D p3d0 = new Point3D();
+        Point3D p3d1 = new Point3D(10, 15, 13);
+
+        System.out.println(p3d1.distance(p3d0));
+        System.out.println(p3d1.distanceOnPlane(p1));
+        System.out.println(p3d1.distance(p1));
     }
 
-    public static void comparePoints(Point p1, Point p2) {
+    public static void comparePoints(Point2D p1, Point2D p2) {
         System.out.println(p1);
         System.out.println(p2);
         System.out.println("Odleglosc miedzy tymi punktami wynosi: " + p1.distance(p2) + "\n");
